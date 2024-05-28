af3b10476fbee03958f141c0fd7e6f2ba905e869
Dylan Machin <dmachin1@sheffield.ac.uk>
2024-05-28 23:52:56 +0100

    Improved logging

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import discord
 import os
+import logging
 from random import choice 
 from dotenv import load_dotenv, dotenv_values 
 
@@ -12,27 +13,31 @@ client = discord.Client(intents=intents)
 
 def match_text_to_image(msg:str) -> list[str]:
     return [x for x in os.listdir("./images") if x[:-4] in msg.split(" ")]
-    return choice(candidates)
-
 
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    logging.info(f'We have logged in as {client.user}')
 
 @client.event
 async def on_message(message):
     mach_list = match_text_to_image(message.content)
-    print(mach_list)
 
-    if message.author == client.user or mach_list == []:
+    if message.author == client.user:
+        return
+
+    logging.debug(f"Found {mach_list} in {message.author}: '{message.content}' from {message.guild.name}")
+
+    if mach_list == []:
         return
 
     image = choice(mach_list)
     with open("./images/"+image, "rb") as fh:
         f = discord.File(fh, filename="./images/"+image)
     await message.channel.send(file=f)
+    logging.info(f"Sent {image} to {message.guild.name} because {message.author} said '{message.content}'!!!")
 
 # loading variables from .env file
 load_dotenv() 
 
+logging.basicConfig(filename="info.log", level=logging.DEBUG)
 client.run(os.getenv("BOT_TOKEN"))
